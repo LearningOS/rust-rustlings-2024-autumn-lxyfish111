731cb36c8c602076cfde2a43e9f589210271fae9
lxyfish111 <lxy_fish_111@126.com>
2024-10-09 21:37:54 +0800

    e8

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +14,14 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut ret = String::new();
+            ret.push(first.to_ascii_uppercase());
+            while let Some(ch) = c.next() {
+                ret.push(ch);
+            }
+            ret
+        },
     }
 }
 
@@ -24,7 +30,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut v:Vec<String> = Vec::new();
+    for item in words.iter() {
+        v.push(capitalize_first(item));
+    }
+    v
 }
 
 // Step 3.
@@ -32,7 +42,15 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut s = String::new();
+    for item in words.iter() {
+        if s.is_empty() {
+            s = format!("{}", capitalize_first(item));
+        } else {
+            s = format!("{}{}", s, capitalize_first(item));
+        }
+    }
+    s
 }
 
 #[cfg(test)]
--- exercises/tests/tests4.rs
+++ exercises/tests/tests4.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Rectangle {
     width: i32,
@@ -35,16 +34,16 @@ mod tests {
     }
 
     #[test]
+    #[should_panic]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
         let _rect = Rectangle::new(-10, 10);
-        assert!(true)
     }
 
     #[test]
+    #[should_panic]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
         let _rect = Rectangle::new(10, -10);
-        assert!(true)
     }
 }
